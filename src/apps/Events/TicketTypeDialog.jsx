bd1e7ba3a0ee10eaa6338386a3b8e1d5a53749a5
Bminor87 <jessehellman@gmail.com>
2024-11-28 23:15:38 +0200

    Fixed error when giving null values to input fields

--- src/apps/Events/TicketTypeDialog.jsx
+++ src/apps/Events/TicketTypeDialog.jsx
@@ -1,7 +1,6 @@
 import { TextField, DialogContent, Stack } from "@mui/material";
 
 export default function TicketTypeDialog({ ticketType, handleChange }) {
-
   return (
     <div>
       <p>{ticketType.id}</p>
@@ -10,24 +9,24 @@ export default function TicketTypeDialog({ ticketType, handleChange }) {
           <TextField
             autoFocus
             required
-            name='name'
-            label='Ticket type name'
-            value={ticketType.name}
+            name="name"
+            label="Ticket type name"
+            value={ticketType.name || ""}
             onChange={handleChange}
           />
           <TextField
             required
-            name='retailPrice'
-            label='Retail price'
-            type='number'
-            value={ticketType.retailPrice}
+            name="retailPrice"
+            label="Retail price"
+            type="number"
+            value={ticketType.retailPrice || ""}
             onChange={handleChange}
           />
           <TextField
-            name='totalTickets'
-            label='Total tickets'
-            type='number'
-            value={ticketType.totalTickets}
+            name="totalTickets"
+            label="Total tickets"
+            type="number"
+            value={ticketType.totalTickets || ""}
             onChange={handleChange}
           />
         </Stack>
